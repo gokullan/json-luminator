e6d12a8b04e9bd992dfe10847d06a718e9a6ca74
Gokulakrishnan S <gokullan1101@gmail.com>
2024-10-20 20:18:49 +0530

    chore: Add gitignore and delete old files

--- saved.js
+++
@@ -1,2 +0,0 @@
-var savedHighlights = 
-  {"highlights":[{"path":[8,1,1,3,0],"anchorOffset":"0","focusOffset":"103","n":0}]}
--- tests/test.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('keydown', (event) => {
-  // use Ctrl+E to trigger function to be tested
-  if (event.ctrlKey && event.key === 'e') {
-  }
-})
